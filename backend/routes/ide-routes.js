2dbfde5740d59f3cf0ffb35a3b83a4a42accdfef
AvaneeshGodra <126774421+AvaneeshGodra@users.noreply.github.com>
2023-10-15 12:13:31 +0530

    Delete backend directory

--- backend/routes/ide-routes.js
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-export const ideRoutes = express.Router();
-ideRoutes.post('/compile');
-ideRoutes.post('/execute');
-ideRoutes.get('/question');
-ideRoutes.get('/questions');
